08566ffa689a7c76bcd0a14308e9786db22d10d8
inti0295 <inti0295@DESKTOP-NU86B0K.home>
2017-02-23 12:18:35 +0100

    Modifié et fini (normalement): Client Commande (avec association)

--- TP_ECOMMERCE/src/fr/adaming/entities/Client.java
+++ TP_ECOMMERCE/src/fr/adaming/entities/Client.java
@@ -1,16 +1,25 @@
 package fr.adaming.entities;
 
+import java.io.Serializable;
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
 @Table(name="clients")
-public class Client {
+public class Client implements Serializable {
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	@Id
 	@Column(name="id_cl")
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
@@ -22,5 +31,82 @@ public class Client {
 	private String email;
 	private String tel;
 	
+	@OneToMany(mappedBy="ClientCommande")
+	private List<Commande> listeCommandes;
+	
+	//Constructors
 
+	public Client() {
+		super();
+	}
+
+	public Client(String nomClient, String adresse, String email, String tel) {
+		super();
+		NomClient = nomClient;
+		this.adresse = adresse;
+		this.email = email;
+		this.tel = tel;
+	}
+
+	public Client(long idClient, String nomClient, String adresse, String email, String tel) {
+		super();
+		this.idClient = idClient;
+		NomClient = nomClient;
+		this.adresse = adresse;
+		this.email = email;
+		this.tel = tel;
+	}
+
+	//Getters and Setters
+	public long getIdClient() {
+		return idClient;
+	}
+
+	public void setIdClient(long idClient) {
+		this.idClient = idClient;
+	}
+
+	public String getNomClient() {
+		return NomClient;
+	}
+
+	public void setNomClient(String nomClient) {
+		NomClient = nomClient;
+	}
+
+	public String getAdresse() {
+		return adresse;
+	}
+
+	public void setAdresse(String adresse) {
+		this.adresse = adresse;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTel() {
+		return tel;
+	}
+
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+
+	public List<Commande> getListeCommandes() {
+		return listeCommandes;
+	}
+
+	public void setListeCommandes(List<Commande> listeCommandes) {
+		this.listeCommandes = listeCommandes;
+	}
+
+	
+	
+	
 }

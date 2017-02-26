15a419d7bb6d720638086097dfcf2e55b6b31797
Tchement <tchement.enseirb@gmail.com>
2017-02-26 23:14:22 +0100

    yolo

--- TP_ECOMMERCE/src/fr/adaming/entities/Produit.java
+++ TP_ECOMMERCE/src/fr/adaming/entities/Produit.java
@@ -42,7 +42,7 @@ public class Produit implements Serializable{
 	private boolean selectionne;
 
 	
-	@ManyToOne(cascade=CascadeType.ALL)
+	@ManyToOne(cascade=CascadeType.MERGE)
 	@JoinColumn(name="categorie_id_fk", referencedColumnName="id_cat")
 	private Categorie categorieProduit;
 
